7989b421e7ec43b7abe840caef438be27fbd418b
Jérôme Wax <jerome.wax@lo2k.net>
2013-08-01 08:20:26 +0200

    Update In5Minutes.js

--- In5Minutes.js
+++ In5Minutes.js
@@ -1,1 +1,16 @@
-alert("hello world");
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too  
+    script = document.createElement( 'script' );  
+   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
+    script.onload=releasetheKraken;  
+    document.body.appendChild(script);  
+}   
+else {  
+    releasetheKraken();  
+}  
+  
+function releasetheKraken() {  
+    // The Kraken has been released, master!    
+    alert("hello world");
+}  
+
+
